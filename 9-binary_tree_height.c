8703c8d25dbed25f480f3f0c48c74c9772923be1
abfabs <albaeftimi@gmail.com>
2025-07-29 10:06:44 +0000

    bettied it

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,28 +2,33 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Height of the tree, or 0 if tree is NULL or has no children
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height = 0;
-    size_t left_edges = 0; 
-    size_t right_edges = 0;
-    
-    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-        return (0);
-    
-    if (tree->left != NULL)
-        left_edges = binary_tree_height(tree->left);
+	size_t height = 0;
+	size_t left_edges = 0;
+	size_t right_edges = 0;
 
-    if (tree->right != NULL)
-        right_edges = binary_tree_height(tree->right);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
-    if (left_edges > right_edges)
-    {
-        height = left_edges + 1;
-        return (height);
-    }
-    
-    height = right_edges + 1;
+	if (tree->left != NULL)
+		left_edges = binary_tree_height(tree->left);
 
-    return (height);
+	if (tree->right != NULL)
+		right_edges = binary_tree_height(tree->right);
+
+	if (left_edges > right_edges)
+	{
+		height = left_edges + 1;
+		return (height);
+	}
+
+	height = right_edges + 1;
+	return (height);
 }
